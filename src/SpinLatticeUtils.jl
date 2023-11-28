dfe679fe8f664594e224b614c41b41ffd2096579
Rui-Hao Bi <biruihao@westlake.edu.cn>
2023-11-28 22:43:53 +0800

    fix dependency

--- src/SpinLatticeUtils.jl
+++ src/SpinLatticeUtils.jl
@@ -5,6 +5,8 @@ using DelimitedFiles
 using Romberg
 # using Interpolations, QuadGK
 
+using AutomaticDocstrings # add AutomaticDocstrings for a developing code
+
 include("files.jl")
 include("SpinLatticeParams.jl")
 include("kernel_integration.jl")
